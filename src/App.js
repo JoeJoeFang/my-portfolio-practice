ceadd88f6f16169d047b06de86ce815d61104322
jiazhoufang <jiazhou.fang@maqro.com.au>
2025-07-09 22:52:29 +1000

    homepage background & profile & mouthEnter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,29 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header>
+      <div className="grid-layout">
+        <div className="grid-item top-left">
+          <img className="pic" src="/images/homepage-background_left_top.jpg" alt="left-top"/>
+        </div>
+        <div className="grid-item top-right">
+          <img className="pic" src="/images/homepage-background_right_top.jpg" alt="right-top"/>
+        </div>
+        <div className="grid-item center">
+          <img className="pic" src="/images/homepage-background_mid.jpg" alt="center"/>
+          <img
+          src={require("./images/profile-pic.jpg")}
+          alt="My portrait"
+          className="avatar"
+        />
+        </div>
+        <div className="grid-item bottom-left">
+          <img className="pic" src="/images/homepage-background_left_bottom.jpg" alt="right-bottom"/>
+        </div>
+        <div className="grid-item bottom-right">
+          <img className="pic" src="/images/homepage-background_right_bottom.jpg" alt="right-bottom"/>
+        </div>
+      </div>
+      {/* <header>
         <p className="homepage-title">Hello, I'm Jiazhou 👋</p>
         <p className="homepage-subtitle">Welcome to my portfolio</p>
         <img
@@ -12,7 +34,7 @@ function App() {
           alt="My portrait"
           className="avatar"
         />
-      </header>
+      </header> */}
     </div>
   );
 }
