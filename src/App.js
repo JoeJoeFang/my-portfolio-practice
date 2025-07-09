a6774924cd13685748a0d9e74c16ff0860b6b97c
jiazhoufang <jiazhou.fang@maqro.com.au>
2025-07-09 15:34:06 +1000

    Initial commit - React portfolio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <p className="homepage-title">Hello, I'm Jiazhou 👋</p>
+        <p className="homepage-subtitle">Welcome to my portfolio</p>
+        <img
+          src={require("./images/profile-pic.jpg")}
+          alt="My portrait"
+          className="avatar"
+        />
       </header>
     </div>
   );
